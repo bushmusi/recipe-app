46799b0d75569c6a4e77fc480d6ca2e63f48e286
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-27 23:54:16 +0430

    add inventory model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_27_175124) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_27_192121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quanity"
@@ -52,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_175124) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inventories", "users"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
