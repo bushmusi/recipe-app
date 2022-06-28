84b9ec61e3512920b9ec4cd98c88b7ead01f46db
bushmusi <bushera.mestofa@gmail.com>
2022-06-28 14:23:46 +0300

    Remove dublicate routes and make recipe default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  get 'inventory_foods/index'
-  get 'inventory_foods/new'
-  get 'foods/index'
-  get 'foods/new'
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/new'
-  get 'recipes/public_recipes'
-  
+  # get 'inventory_foods/index'
+  # get 'inventory_foods/new'
+  # get 'foods/index'
+  # get 'foods/new'
+  # get 'recipes/index'
+  # get 'recipes/show'
+  # get 'recipes/new'
+  # get 'recipes/public_recipes'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  root to: "home#index"
-  # root 'recipes#index'
-  
+  # root to: "home#index"  # 
+  root 'recipes#index'
+
   resources :recipes, only: [:index, :show, :new]
   resources :foods, only: [:index, :show, :new]
   resources :inventory_foods, only: [:index, :new]
