dd3da03569d7700965d21ad1bdb04658cdc3ba77
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-28 13:05:41 +0430

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_27_203647) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_083302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,12 +35,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_203647) do
   end
 
   create_table "inventory_foods", force: :cascade do |t|
+    t.decimal "quantity"
+    t.bigint "inventory_id", null: false
+    t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
+    t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.integer "quanity"
+    t.decimal "quanity"
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
@@ -82,6 +87,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_203647) do
   add_foreign_key "foods", "foods"
   add_foreign_key "foods", "users"
   add_foreign_key "inventories", "users"
+  add_foreign_key "inventory_foods", "foods"
+  add_foreign_key "inventory_foods", "inventories"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
