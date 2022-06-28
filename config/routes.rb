07830796df6e91895f5a596376b43768dc8b089c
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-06-28 14:30:56 +0430

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'inventory_foods/index'
+  get 'inventory_foods/new'
+  get 'foods/index'
+  get 'foods/new'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/public_recipes'
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  root to: "home#index"
+  # root to: "home#index"
+  root 'recipes#index'
+  
+  resources :recipes, only: [:index, :show, :new]
+  resources :foods, only: [:index, :show, :new]
+  resources :inventory_foods, only: [:index, :new]
 end
