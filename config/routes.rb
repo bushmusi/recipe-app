6c511b04f25d8712379d2bea3d92fdb0db22d7f4
bushmusi <bushera.mestofa@gmail.com>
2022-06-29 10:49:29 +0300

    remove unseccasry lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
-  # get 'inventory_foods/index'
-  # get 'inventory_foods/new'
-  # get 'foods/index'
-  # get 'foods/new'
-  # get 'recipes/index'
-  # get 'recipes/show'
-  # get 'recipes/new'
-  # get 'recipes/public_recipes'
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # root to: "home#index"  #
   root 'recipes#index'
 
   resources :recipes, only: [:index, :show, :new]
   resources :foods, only: [:index, :show, :new, :create]
   resources :inventory_foods, only: [:index, :new]
+  resources :dogs
 end
