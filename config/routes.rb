c5c1184089472825896a5352d6feecfe4e3ad69e
bushmusi <bushera.mestofa@gmail.com>
2022-06-29 15:12:04 +0300

    Add destroy action to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "recipes#public_recipes"
 
-  # or we can have a homepage at the start: "users#index"
+  devise_for :users
+  root 'recipes#index'
 
   resources :recipes, only: [:index, :show, :new]
-  resources :foods, only: [:index, :show, :new]
-  # resources :inventory_foods, only: [:index, :new]
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :inventory_foods, only: [:index, :new]
 end
