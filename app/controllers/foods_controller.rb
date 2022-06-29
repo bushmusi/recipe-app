4d41f4c2a765241f12e1be460032f6eda4f8f008
bushmusi <bushera.mestofa@gmail.com>
2022-06-29 15:11:04 +0300

    Food controler update

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,30 @@
 class FoodsController < ApplicationController
-  def index; end
+  before_action :current_food, only: [:destroy]
+  protect_from_forgery prepend: true
+  def index
+    @foods = Food.all.order(name: :asc)
+    @food = Food.new
+  end
 
-  def new; end
-  def create; end
+  def create
+    temp = Food.create(food_params)
+    puts "-------#{temp.id}"
+    puts "-------#{food_params}"
+    redirect_to foods_path
+  end
+
+  def destroy
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :user_id)
+  end
+
+  def current_food
+    @food = Food.find(params[:id])
+  end
 end
