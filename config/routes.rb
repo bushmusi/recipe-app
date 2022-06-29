9858814c8b873dee4ad46a7f1aec401c6134abf9
bushmusi <bushera.mestofa@gmail.com>
2022-06-29 15:38:28 +0300

    Fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "recipes#public_recipes"
 
-  devise_for :users
-  root 'recipes#index'
-
   resources :recipes, only: [:index, :show, :new]
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   resources :inventory_foods, only: [:index, :new]
