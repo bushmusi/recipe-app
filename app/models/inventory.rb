a4119c7a3e6f6ab087c3c5b409beecfc1eebc60c
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-30 10:06:31 +0200

    Change model Inventory details by adding description member string and add tests to validate it

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,5 +1,5 @@
 class Inventory < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :name
+  validates_presence_of :name, :description
 end
--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Inventory, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
   end
 
   describe 'relationships' do
